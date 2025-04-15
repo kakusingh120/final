544bf6a0d249679f9f958c3a250b192e6670f5c1
krish singh <154358180+kakusingh120@users.noreply.github.com>
2025-04-15 14:02:23 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const dbconnect=async()=>{
     try{
         const URI = `${process.env.db_uri}/vidtube`
+         mongoose.set('strictQuery', false);
       const connection  =  await  mongoose.connect(URI)
           console.log("Connected to the database:", connection.connection.host);
     }
